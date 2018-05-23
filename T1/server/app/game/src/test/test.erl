93c73b66f15c5a5d291e60b175dea9c22f356cdd
snailman <mawh583@163.com>
2018-05-23 11:17:33 +0800

    auto commit

--- T1/server/app/game/src/test/test.erl
+++ T1/server/app/game/src/test/test.erl
@@ -18,8 +18,21 @@ tv3()->
     P2 = #vector3{x = 100.1, z = 50.1},
     P3 = #vector3{x = 100.1, z = 150.1},
     io:format("normalized ~w -> ~w~n",[P1, vector3:normalized(P1)]),
-    io:format("behind     ~w -> ~w~n",[P1, vector3:behind(P1, P2, 10)]),
-    io:format("before     ~w -> ~w~n",[P1, vector3:before(P1, P2, 10)]),
-    io:format("P1 is_before P2 -> ~w~n", [vector3:is_behind(P1, P2, vector3:subtract(P1, P2))]),
-    io:format("P1 is_before P2 -> ~w~n", [vector3:is_behind(P1, P2, vector3:subtract(P2, P1))]),
+
+    tv3_1(P1, P2),
+    tv3_1(P1, P3),
+    tv3_1(P2, P1),
+    tv3_1(P2, P3),
+    tv3_1(P3, P1),
+    tv3_1(P3, P2),
+    ok.
+
+tv3_1(Src, Dst) ->
+    io:format("~n"),
+    io:format("src:~w~n",[Src]),
+    io:format("dst:~w~n",[Dst]),
+    io:format("\tbehind     ~w -> ~w~n",[Src, vector3:behind(Src, Dst, 10)]),
+    io:format("\tbefore     ~w -> ~w~n",[Src, vector3:before(Src, Dst, 10)]),
+    io:format("\tSrc is_before P2 -> ~w~n", [vector3:is_behind(Src, Dst, vector3:subtract(Src, Dst))]),
+    io:format("\tSrc is_before P2 -> ~w~n", [vector3:is_behind(Src, Dst, vector3:subtract(Dst, Src))]),
     ok.
