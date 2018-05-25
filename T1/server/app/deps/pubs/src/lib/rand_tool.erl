0451a82b0666fc21fff0fb7ec2c006c30ad88ffc
snailman <mawh583@163.com>
2018-05-25 16:56:49 +0800

    auto commit

---
+++ T1/server/app/deps/pubs/src/lib/rand_tool.erl
@@ -0,0 +1,50 @@
+%%%-------------------------------------------------------------------
+%%% @author mawenhong
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%  伪随机
+%%% @end
+%%% Created : 25. 五月 2018 16:39
+%%%-------------------------------------------------------------------
+-module(rand_tool).
+-author("mawenhong").
+
+%% API
+-export([new/0]).
+-export([new/1]).
+-export([rand/0]).
+-export([seed/0]).
+
+
+-define(INT32_MAX, 2147483647).
+-define(RAND_KEY, fake_rand_process_key___).
+
+-spec new() -> integer().
+new() ->
+    Key = os:system_time(seconds),
+    put(?RAND_KEY, Key),
+    Key.
+
+new(Seed) ->
+    put(?RAND_KEY, Seed),
+    Seed.
+
+-spec rand() -> integer().
+rand() ->
+    ensure_new(),
+    Key1 = get(?RAND_KEY),
+    % gcc
+    Key2 = ((Key1 * 1103515245 + 12345) bsl 1) band ?INT32_MAX ,
+    put(?RAND_KEY, Key2),
+    Key2.
+
+seed() ->
+    ensure_new(),
+    get(?RAND_KEY).
+%%
+ensure_new() ->
+    case get(?RAND_KEY) of
+        undefined -> new();
+        _ -> skip
+    end.
+
