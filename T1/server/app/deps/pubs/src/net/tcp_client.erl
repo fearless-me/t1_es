8ba9f4cfac0bf56a6ab1a232af0b3f034ca0407e
snailman <mawh583@163.com>
2018-06-04 18:21:53 +0800

    auto commit

--- T1/server/app/deps/pubs/src/net/tcp_client.erl
+++ T1/server/app/deps/pubs/src/net/tcp_client.erl
@@ -43,7 +43,7 @@ connect(Port, MapID) ->
     timer:sleep(50),
     recv_msg(Socket),
 %%    send_msg(Socket, #pk_GS2U_GoNewMap{tarMapID = MapID, fX = misc:rand(500, 5000) / 10, fY = misc:rand(500, 3000) / 10}),
-    timer:sleep(5 * 60 * 1000),
+    timer:sleep(15 * 60 * 1000),
     ok.
 
 send_msg(Socket, Msg) ->
