e423293e0d412cf448153d129f484d50bb62e190
snailman <mawh583@163.com>
2018-06-19 21:15:10 +0800

    auto commit

--- T1/server/app/game/src/hook/hook_map.erl
+++ T1/server/app/game/src/hook/hook_map.erl
@@ -18,17 +18,26 @@
 %%-------------------------------------------------------------------
 %%不要在调用lib_obj_rw:set_xxx
 %%-------------------------------------------------------------------
+-define(lock(X), lock_transcation(X)).
+-define(unlock(), unlock_transcation()).
+
 on_rw_update(Uid, hp, Hp) ->
+    ?lock({Uid, hp}),
     Type = lib_obj_rw:get_type(Uid),
     on_rw_update_pub_action(Type, Uid, {#m_player_pub.hp, Hp}),
+    ?unlock(),
     ok;
 on_rw_update(Uid, attr, Attrs) ->
+    ?lock({Uid, attr}),
     Type = lib_obj_rw:get_type(Uid),
     on_rw_update_priv_action(Type, Uid, {#m_player_private.priv_attrs, Attrs}),
+    ?unlock(),
     ok;
 on_rw_update(Uid, buff_list, BuffList) ->
+    ?lock({Uid, buff_list}),
     Type = lib_obj_rw:get_type(Uid),
     on_rw_update_priv_action(Type, Uid, {#m_player_private.priv_buffs, BuffList}),
+    ?unlock(),
     ok;
 on_rw_update(_Uid, _Key, _Value) ->
     ok.
@@ -47,3 +56,16 @@ on_rw_update_priv_action(?OBJ_USR, Uid, Element) ->
     ok;
 on_rw_update_priv_action(_ObjType, _Uid, _Element) ->
     ok.
+
+
+%%-------------------------------------------------------------------
+lock_transcation(Key)->
+    case get(map_obj_lock_transcation) of
+        Key -> throw("recursive call");
+        _ -> skip
+    end,
+    ok.
+
+unlock_transcation()->
+    erase(map_obj_lock_transcation).
+
