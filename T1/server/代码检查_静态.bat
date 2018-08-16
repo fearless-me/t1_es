c9b3ee92a7b798d6e8a0976c9344db9569ad3f86
snailman <mawh583@163.com>
2018-08-16 17:34:18 +0800

    auto commit

--- T1/server/代码检查_静态.bat
+++ T1/server/代码检查_静态.bat
@@ -5,4 +5,6 @@ dialyzer --build_plt --apps erts kernel stdlib mnesia
 @dialyzer -r ./app/game/ebin -o ./app/dialyzer-game.log   --fullpath --dump_callgraph callg.raw
 echo check game code
 dialyzer -r ./app/game/src ./app/deps/pubs/src --src -o ./dialyzer-game.log   --fullpath --dump_callgraph callg.raw
+echo check center code
+dialyzer -r ./app/center/src 	./app/deps/pubs/src ./app/deps/db_proxy/src ./app/deps/db_share/src --src -o ./dialyzer-game.log   --fullpath --dump_callgraph callg.raw
 pause
