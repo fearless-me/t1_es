0c3bcd6522fd74831f1a97a5b44c2de544168675
snailman <mawh583@163.com>
2018-08-22 20:52:48 +0800

    auto commit

--- T1/server/app/center/src/dist/cs_dist.erl
+++ T1/server/app/center/src/dist/cs_dist.erl
@@ -17,21 +17,30 @@
 -define(Wrap(X), fun() -> X end).
 
 start_master() ->
-    misc_dist:start_slave('127.0.0.1', center_logic, " -s dist start "),
+    ?WARN("try to start master node ..."),
+    misc_dist:start_slave('127.0.0.1', cg, " -s dist start "),
+    misc_dist:start_slave('127.0.0.1', ct, " -s dist start "),
+    misc_dist:start_slave('127.0.0.1', cf, " -s dist start "),
     ok.
 
 start_slave(SupPid) ->
     try
-        wrapper({"logger", stdio, ?Wrap(cs_start_fn:start_logs(SupPid))}),
-        wrapper({"error Logger", ?Wrap(cs_start_fn:start_errlog(SupPid))}),
-        wrapper({"gen rpc app", ?Wrap(cs_start_fn:start_rpc(SupPid))}),
-        wrapper({"create dist_exam_otp", ?Wrap(cs_start_fn:start_slave_otp(SupPid, dist_exam_otp))}),
+        wrapper({"logger", stdio,   ?Wrap(cs_start_fn:start_logs(SupPid))}),
+        wrapper({"error Logger",    ?Wrap(cs_start_fn:start_errlog(SupPid))}),
+        wrapper({"gen rpc app",     ?Wrap(cs_start_fn:start_rpc(SupPid))}),
+        wrapper({"start logic otp", ?Wrap(start_slave_otp(node(), SupPid))}),
         ok
     catch _ : Err : ST ->
         misc:halt("start app error ~p, stacktrace ~p", [Err, ST])
     end,
-    ?WARN("###~p start ok###", [node()]),
+    ?WARN("###slave ~p start ok###", [node()]),
     ok.
+
+start_slave_otp('cg@127.0.0.1', SupPid)->
+    wrapper({"create dist_exam_otp", ?Wrap(cs_start_fn:start_slave_otp(SupPid, dist_exam_otp))}),
+    ok;
+start_slave_otp(_Node, _SupPid) ->
+    skip.
 
 
 wrapper({Msg, stdio, Thunk}) ->
