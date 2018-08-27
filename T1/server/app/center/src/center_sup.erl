54ce50dfa7f943be01605284f1b36dc2849933e7
snailman <mawh583@163.com>
2018-08-27 20:03:01 +0800

    auto commit

--- T1/server/app/center/src/center_sup.erl
+++ T1/server/app/center/src/center_sup.erl
@@ -35,19 +35,21 @@ start() ->
     try
         misc:fn_wrapper({"logger", stdio,           ?Wrap(loggerS:start())}),
         misc:fn_wrapper({"error Logger",            ?Wrap(common_error_logger:start(center_sup, center))}),
-        misc:fn_wrapper({"watchdog",                ?Wrap(misc:start_otp(SupPid, watchdog, worker, [fun cs_watchdog_hook:task_list/0]))}),
         misc:fn_wrapper({"gen rpc app",             ?Wrap(misc:start_all_app(gen_rpc))}),
-        misc:fn_wrapper({"center dist otp",         ?Wrap(misc:start_otp(SupPid, cs_dist_otp, worker))}),
         misc:fn_wrapper({"config init",             ?Wrap(cs_conf:start("center.ini"))}),
+        misc:fn_wrapper({"db share",                ?Wrap(cs_share:start())}),
+        misc:fn_wrapper({"db window",               ?Wrap(cs_db_starter:start())}),
+        misc:fn_wrapper({"auto compile and load",   ?Wrap(fly:start())}),
+
+        misc:fn_wrapper({"center dist otp",         ?Wrap(misc:start_otp(SupPid, cs_dist_otp, worker))}),
+        misc:fn_wrapper({"watchdog",                ?Wrap(misc:start_otp(SupPid, watchdog, worker, [fun cs_watchdog_hook:task_list/0]))}),
         misc:fn_wrapper({"monitor/gc/vms",          ?Wrap(misc:start_otp(SupPid, background_gc, worker))}),
         misc:fn_wrapper({"monitor/gc/vms",          ?Wrap(misc:start_otp(SupPid, vm_memory_monitor, worker, [0.5]))}),
         misc:fn_wrapper({"system monitor",          ?Wrap(misc:start_otp(SupPid, system_monitor, worker))}),
         misc:fn_wrapper({"serv_cache",              ?Wrap(misc:start_otp(SupPid, cs_cache_otp, worker))}),
-        misc:fn_wrapper({"db share",                ?Wrap(cs_share:start())}),
-        misc:fn_wrapper({"db window",               ?Wrap(cs_db_starter:start())}),
         misc:fn_wrapper({"serv data loader",        ?Wrap(misc:start_otp(SupPid, cs_loader_otp, worker))}),
         misc:fn_wrapper({"all logic process",       ?Wrap(misc:start_otp(SupPid, cs_logic_sup, supervisor))}),
-        misc:fn_wrapper({"auto compile and load",   ?Wrap(fly:start())}),
+        
         misc:fn_wrapper({"distritbution",           ?Wrap(start_dist(SupPid))}),
             
         watchdog:wait(),
--- T1/server/app/game/src/game_sup.erl
+++ T1/server/app/game/src/game_sup.erl
@@ -33,25 +33,28 @@ start() ->
     {ok, SupPid} = game_sup:start_link(),
 
     try
+        %%          
         misc:fn_wrapper({"logger", stdio,           ?Wrap(loggerS:start())}),
         misc:fn_wrapper({"error Logger",            ?Wrap(common_error_logger:start(game_sup, game))}),
-        misc:fn_wrapper({"watchdog",                ?Wrap(misc:start_otp(SupPid, watchdog, worker, [fun gs_watchdog_hook:task_list/0]))}),
         misc:fn_wrapper({"gen rpc app",             ?Wrap(misc:start_all_app(gen_rpc))}),
         misc:fn_wrapper({"config init",             ?Wrap(gs_conf:start("game.ini"))}),
+        misc:fn_wrapper({"db share",                ?Wrap(gs_share:start())}),
+        misc:fn_wrapper({"db window",               ?Wrap(gs_db_starter:start())}),
+        misc:fn_wrapper({"auto compile and load",   ?Wrap(fly:start())}),
+
+        %%
+        misc:fn_wrapper({"watchdog",                ?Wrap(misc:start_otp(SupPid, watchdog, worker, [fun gs_watchdog_hook:task_list/0]))}),
         misc:fn_wrapper({"monitor/gc/vms",          ?Wrap(misc:start_otp(SupPid, background_gc, worker))}),
         misc:fn_wrapper({"monitor/gc/vms",          ?Wrap(misc:start_otp(SupPid, vm_memory_monitor, worker, [0.5]))}),
         misc:fn_wrapper({"system monitor",          ?Wrap(misc:start_otp(SupPid, system_monitor, worker))}),
         misc:fn_wrapper({"map root supervisor",     ?Wrap(misc:start_otp(SupPid, gs_map_root_sup, supervisor))}),
         misc:fn_wrapper({"login window",            ?Wrap(misc:start_otp(SupPid, login_otp, worker))}),
         misc:fn_wrapper({"serv_cache",              ?Wrap(misc:start_otp(SupPid, gs_cache_otp, worker))}),
-        misc:fn_wrapper({"db share",                ?Wrap(gs_share:start())}),
-        misc:fn_wrapper({"db window",               ?Wrap(gs_db_starter:start())}),
         misc:fn_wrapper({"broadcast mod",           ?Wrap(misc:start_otp(SupPid, gs_broadcast_otp, worker))}),
         misc:fn_wrapper({"serv data loader",        ?Wrap(misc:start_otp(SupPid, gs_loader_otp, worker))}),
         misc:fn_wrapper({"all logic process",       ?Wrap(misc:start_otp(SupPid, gs_logic_sup, supervisor))}),
-
-        misc:fn_wrapper({"auto compile and load",   ?Wrap(fly:start())}),
         misc:fn_wrapper({"center window process",   ?Wrap(misc:start_otp(SupPid, gs_cs_otp, worker))}),
+            
         watchdog:wait(),
 
         misc:fn_wrapper({"test network 15555",      ?Wrap(start_listener_15555(SupPid))}),
