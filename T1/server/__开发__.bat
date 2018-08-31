4dadbba6c56a0a03b2c47210494e268aef3a855c
snailman <mawh583@163.com>
2018-08-31 14:53:37 +0800

    auto commit

--- T1/server/__开发__.bat
+++ T1/server/__开发__.bat
@@ -1,8 +1,8 @@
 @echo off
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
-cd /d "%~dp0"
+rem %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+rem cd /d "%~dp0"
 
 cls
 cd scripts
-
+erl -make
 escript.exe dev.ers
