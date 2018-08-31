b522427156c3634e9b40d96462f6e14e9387b654
snailman <mawh583@163.com>
2018-08-31 16:13:14 +0800

    auto commit

--- T1/server/scripts/dialyzer_tool.bat
+++ T1/server/scripts/dialyzer_tool.bat
@@ -1,14 +1,14 @@
 cls
 @echo off
 echo build plt
-@dialyzer --build_plt --apps erts kernel stdlib mnesia
+dialyzer.exe --build_plt --apps erts kernel stdlib mnesia
 
 echo check game code
-dialyzer -r ../app/game/ebin -o ../app/dialyzer-game.log   --fullpath --dump_callgraph callg.raw
+dialyzer.exe -r ../app/game/ebin -o ../app/dialyzer-game.log   --fullpath --dump_callgraph callg.raw
 @dialyzer -r ../app/game/src ../app/deps/pubs/src --src -o ../dialyzer-game.log   --fullpath --dump_callgraph callg.raw
 
 
 echo check center code
-dialyzer -r ../app/center/ebin -o ../app/dialyzer-center.log   --fullpath --dump_callgraph callc.raw
+dialyzer.exe -r ../app/center/ebin -o ../app/dialyzer-center.log   --fullpath --dump_callgraph callc.raw
 @dialyzer -r ../app/center/src 	../app/deps/pubs/src ../app/deps/db_proxy/src ../app/deps/db_share/src --src -o ../dialyzer-center.log   --fullpath --dump_callgraph callc.raw
 pause
--- T1/server/scripts/xref.bat
+++ T1/server/scripts/xref.bat
@@ -1,5 +1,6 @@
 cls
 @echo off
 cd xref_runner
-start /B /wait erl -s  xref_runner check
+start /B /wait erl -s  xref_runner check "..\\xref_gs.config"
+start /B /wait erl -s  xref_runner check "..\\xref_cs.config"
 pause
