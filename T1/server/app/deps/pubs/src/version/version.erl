2795777e96e387a5ee7589063d0cab527c7ec1a1
snailman <mawh583@163.com>
2018-08-31 15:13:12 +0800

    auto commit

--- T1/server/app/deps/pubs/src/version/version.erl
+++ T1/server/app/deps/pubs/src/version/version.erl
@@ -1,9 +1,9 @@
-%%NewVersion:111245
-%%OldVersion:111244
+%%NewVersion:111247
+%%OldVersion:111246
 %%this file auto generate !!!
 %%this file auto generate !!!
 %%this file auto generate !!!
-%%UpdateTime:2018-08-31 15:08:30
+%%UpdateTime:2018-08-31 15:11:10
 -module(version).
 -export([getVersion/0]).
-getVersion() -> 111245.
+getVersion() -> 111247.
