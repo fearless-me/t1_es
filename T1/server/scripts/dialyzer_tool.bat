259dc40926b87f9d2420aeb7907b5863cb35c59c
snailman <mawh583@163.com>
2018-08-31 16:34:14 +0800

    auto commit

--- T1/server/scripts/dialyzer_tool.bat
+++ T1/server/scripts/dialyzer_tool.bat
@@ -1,14 +1,14 @@
 cls
 @echo off
 echo build plt
-@dialyzer.exe --build_plt --apps erts kernel stdlib mnesia
+dialyzer.exe --build_plt --apps erts kernel stdlib mnesia
 
 echo check game code
-dialyzer.exe --gui -r ../app/game/ebin -I "../app/game/include" -I "../app/game/src/pub/config" -I "../app/game/src/pub/netmsg" -I "../app/common_include" -I "../app/deps/pubs/include" -I "../app/deps/gen_rpc/include" -I "../app/deps/mysql-otp/include"  -I "../app/center/include"  -o ../app/dialyzer-game.log   --fullpath --dump_callgraph callg.raw
-@dialyzer -r ../app/game/src ../app/deps/pubs/src --src -I "../app/game/include" -I "../app/game/src/pub/config" -I "../app/game/src/pub/netmsg" -I "../app/common_include" -I "../app/deps/pubs/include" -I "../app/deps/gen_rpc/include" -I "../app/deps/mysql-otp/include"  -I "../app/center/include"  -o ../dialyzer-game.log   --fullpath --dump_callgraph ./callg.raw
+@dialyzer.exe -r ../app/game/ebin -I "../app/game/include" -I "../app/game/src/pub/config" -I "../app/game/src/pub/netmsg" -I "../app/common_include" -I "../app/deps/pubs/include" -I "../app/deps/gen_rpc/include" -I "../app/deps/mysql-otp/include"  -I "../app/center/include"  -o ../app/dialyzer-game.log   --fullpath --dump_callgraph callg.raw
+dialyzer -r ../app/game/src ../app/deps/pubs/src --src -I "../app/game/include" -I "../app/game/src/pub/config" -I "../app/game/src/pub/netmsg" -I "../app/common_include" -I "../app/deps/pubs/include" -I "../app/deps/gen_rpc/include" -I "../app/deps/mysql-otp/include"  -I "../app/center/include"  -o ../dialyzer-game.log   --fullpath --dump_callgraph ./callg.raw
 
 
 echo check center code
-dialyzer.exe --gui -r ../app/center/ebin -I "../app/game/include" -I "../app/game/src/pub/config" -I "../app/game/src/pub/netmsg" -I "../app/common_include" -I "../app/deps/pubs/include" -I "../app/deps/gen_rpc/include" -I "../app/deps/mysql-otp/include"  -I "../app/center/include"  -o ../app/dialyzer-center.log   --fullpath --dump_callgraph callc.raw
-@dialyzer -r ../app/center/src 	../app/deps/pubs/src ../app/deps/db_proxy/src ../app/deps/db_share/src --src -I "../app/game/include" -I "../app/game/src/pub/config" -I "../app/game/src/pub/netmsg" -I "../app/common_include" -I "../app/deps/pubs/include" -I "../app/deps/gen_rpc/include" -I "../app/deps/mysql-otp/include"  -I "../app/center/include"  -o ../dialyzer-center.log   --fullpath --dump_callgraph ./callc.raw
+@dialyzer.exe -r ../app/center/ebin -I "../app/game/include" -I "../app/game/src/pub/config" -I "../app/game/src/pub/netmsg" -I "../app/common_include" -I "../app/deps/pubs/include" -I "../app/deps/gen_rpc/include" -I "../app/deps/mysql-otp/include"  -I "../app/center/include"  -o ../app/dialyzer-center.log   --fullpath --dump_callgraph callc.raw
+dialyzer -r ../app/center/src 	../app/deps/pubs/src ../app/deps/db_proxy/src ../app/deps/db_share/src --src -I "../app/game/include" -I "../app/game/src/pub/config" -I "../app/game/src/pub/netmsg" -I "../app/common_include" -I "../app/deps/pubs/include" -I "../app/deps/gen_rpc/include" -I "../app/deps/mysql-otp/include"  -I "../app/center/include"  -o ../dialyzer-center.log   --fullpath --dump_callgraph ./callc.raw
 pause
