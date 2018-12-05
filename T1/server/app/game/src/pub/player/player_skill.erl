f296a557a893414a1f9611e948f5f83f1241ccba
snailman <mawh583@163.com>
2018-12-05 18:21:50 +0800

    auto commit

---
+++ T1/server/app/game/src/pub/player/player_skill.erl
@@ -0,0 +1,71 @@
+%%%-------------------------------------------------------------------
+%%% @author tiancheng
+%%% @copyright (C) 2018, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 03. 十二月 2018 16:09
+%%%-------------------------------------------------------------------
+-module(player_skill).
+-author("tiancheng").
+
+-include("object.hrl").
+-include("logger.hrl").
+-include("cfg_skill.hrl").
+-include("netmsg.hrl").
+-include("combat.hrl").
+
+%% API
+-export([
+    get_skill_info_list/0
+]).
+
+get_skill_info_list() ->
+    Career = player_rw:get_career(),
+
+    %% TODO 临时代码，上线初始化技能
+    F =
+        fun(ID, {SlotIndex, AccL}) ->
+            #skillCfg{id = SkillID, career = C, subType = SubType, it_type = ItType}
+                = getCfg:getCfgByKey(cfg_skill, ID),
+            case SubType of
+                ?SKILL_SUB_TYPE_IT ->
+                    case C =:= Career of
+                        true ->
+                            case ItType of
+                                ?SKILL_SUB_TYPE_IT_NORMAL ->
+                                    {SlotIndex,
+                                        [#pk_SkillInfo{
+                                            skill_id = SkillID,
+                                            level = 1,
+                                            cd_time = 0,
+                                            slot_index = 0
+                                        } | AccL]};
+                                _ ->
+                                    case SlotIndex > 3 of
+                                        false ->
+                                            {SlotIndex + 1,
+                                                [#pk_SkillInfo{
+                                                    skill_id = SkillID,
+                                                    level = 1,
+                                                    cd_time = 0,
+                                                    slot_index = SlotIndex
+                                                } | AccL]};
+                                        _ ->
+                                            SlotIndex, AccL
+                                    end
+                            end;
+                        _ ->
+                            {SlotIndex, AccL}
+                    end;
+                _ ->
+                    {SlotIndex, AccL}
+            end
+        end,
+    case lists:foldl(F, {1, []}, getCfg:get1KeyList(cfg_skill)) of
+        {_, []} ->
+            ?ERROR("role no skill:~p,~p", [player_rw:get_uid(), Career]),
+            [];
+        {_, SkillList} ->
+            SkillList
+    end.
