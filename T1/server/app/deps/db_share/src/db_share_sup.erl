d3d32185e6ae4bc015f293128e36c0bfe18a169c
snailman <mawh583@163.com>
2019-01-11 11:00:00 +0800

    auto commit

--- T1/server/app/deps/db_share/src/db_share_sup.erl
+++ T1/server/app/deps/db_share/src/db_share_sup.erl
@@ -2,16 +2,13 @@
 
 -behaviour(supervisor).
 
+-include("pub_def.hrl").
+
 %% API
 -export([start_link/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
-
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
--define(CHILD(I, Type, Params), {I, {I, start_link, Params}, permanent, 5000, Type, [I]}).
--define(CHILD(Name, I, Type, Params), {Name, {I, start_link, Params}, permanent, 5000, Type, [Name]}).
 
 %% ===================================================================
 %% API functions
